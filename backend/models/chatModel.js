0e5967342ef1f3e6c1c17c124e34182c6d9f1075
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-05-14 20:16:24 +0530

    Message API created
    Message send api created  for one on one and Group chat

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -3,27 +3,27 @@ const mongoose = require('mongoose')
 const chatModel = mongoose.Schema(
 
     {
-        chatName:{type:String,trim:true},
-        isGroupChat:{type:Boolean, default: false},
-        users:[{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:'User',
+        chatName: { type: String, trim: true },
+        isGroupChat: { type: Boolean, default: false },
+        users: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
         },
-    ],
-    latestMessage: {
-         type:mongoose.Schema.Types.ObjectId,
-         ref: 'Message'
-    },
-    groupAdmin: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
+        ],
+        latestMessage: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Message'
+        },
+        groupAdmin: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
     },
     {
-        timestamps:true,
+        timestamps: true,
     }
-    );
+);
 
-    const chat = mongoose.model('chat', chatModel);
+const Chat = mongoose.model('Chat', chatModel);
 
-    module.exports = chat;
+module.exports = Chat;
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ const connectDB = require('./config/db');
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes")
 const { notFound, errorHandler } = require("./middleware/errorMiddleware")
+
 // const cors = require('cors')
 // app.use(cors())
 
@@ -21,6 +23,8 @@ app.get('/', (req, res) => {
 
 app.use('/api/user', userRoutes)
 app.use('/api/chat', chatRoutes)
+app.use('/api/message', messageRoutes)
+
 
 app.use(notFound)
 app.use(errorHandler)
